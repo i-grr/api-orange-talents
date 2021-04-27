5bb39a4839df4cd3d73fb81edad92db4ec175c93
Igor Pereira <igorpereira120@gmail.com>
2021-04-27 02:13:23 -0300

    Project created

---
+++ src/test/java/io/github/i_grr/api/orange/talents/ApiOrangeTalentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.i_grr.api.orange.talents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrangeTalentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
